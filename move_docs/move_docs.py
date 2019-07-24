303a9af4e78a5f8d8041921aeb8d1e77a7816a78
Shiva Shams <shams@parspooyesh.com>
2019-07-24 11:47:25 +0430

    import and user config file

--- move_docs/move_docs.py
+++ move_docs/move_docs.py
@@ -1,18 +1,25 @@
 from pymongo import MongoClient
+import configparser
 import pymongo
 import time
 
-src_mongo_host = "10.104.70.11:27019"
-src_collection = "connection_log"
-src_db = "IBSng"
 
-dst_mongo_host = "127.0.0.1:27019"
-dst_collection = "connection_log"
-dst_db = "IBSng"
+cfg = configparser.ConfigParser()
+cfg.read('config_file.cfg')
+
+src_mongo_host = cfg.get('DB', 'src_host')
+src_collection = cfg.get('Collection', 'src_col')
+src_db = cfg.get('DB', 'src_db')
+
+dst_mongo_host = cfg.get('DB', 'dst_host')
+dst_collection = cfg.get('Collection', 'dst_col')
+dst_db = cfg.get('DB', 'dst_db')
 
 from datetime import datetime
-start_logout_time = datetime(2019, 6, 14, 0, 0, 0)
-end_logout_time = datetime(2019, 6, 18, 0, 0, 0)
+st_logout = cfg.get('Logout', 'start').split(',')
+end_logout = cfg.get('Logout', 'end').split(',')
+start_logout_time = datetime(int(st_logout[0]), int(st_logout[1]), int(st_logout[2]), int(st_logout[3]), int(st_logout[4]), int(st_logout[5]))
+end_logout_time = datetime(int(end_logout[0]), int(end_logout[1]), int(end_logout[2]), int(end_logout[3]), int(end_logout[4]), int(end_logout[5]))
 
 try:
     src_conn = MongoClient(src_mongo_host)
@@ -37,7 +44,7 @@ bulk = []
 for doc in src_coll.find(src_query):
     connection_id = int(doc["_id"])
     user_id = int(doc["user_id"])
-    imsi = doc["username"]
+    username = doc["username"]
     dst_query = {"_id": connection_id, "user_id": user_id}
 
     dst_result =  dst_coll.find(dst_query).count()
@@ -45,20 +52,19 @@ for doc in src_coll.find(src_query):
         continue
 
     print("====================Adding to bulk=====================")
-    print(connection_id, user_id, imsi)
-    print(" ======================================================")
+    print(connection_id, user_id, username)
     bulk.append(doc)
     if len(bulk) == 1000:
         try:
-            dst_coll.insert_many(bulk)
             print("===================== inserting bulk =========================")
+            dst_coll.insert_many(bulk)
             bulk = []
 
-            time.sleep(0.1)
+            time.sleep(0.3)
         except Exception as Error:
             print("##################Error in insert action#################", str(Error))
 if bulk:
     print("================ Inserting final bulk ================")
     dst_coll.insert_many(bulk)
-    print("=================== End ===================")
+    print("=================== Fnish ===================")
 
